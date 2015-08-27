a54c1d12785d0bd58d516816f932b43df3a61c32
drewhutchison <autoincandescent@gmail.com>
2015-08-26 21:41:33 -0600

    removed python code

--- code/implementations/min.py
+++
@@ -1,2 +0,0 @@
-solution=lambda p:(lambda s:len(s)-s.index(p[s[-1]]))(reduce(lambda s,d:s+[(p[s[-1]])] if p[s[-1]] not in s else s,p,[0]))
-
--- code/implementations/signiture.py
+++
@@ -1,5 +0,0 @@
-# prints function argument and returns NaN
-
-def solution(pirates):
-  print pirates
-  return float('NaN')
